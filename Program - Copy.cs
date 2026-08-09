97b04941d28eb7c9dcc45c44cb4e99263cec6e3b
lekxna <143764487+lekxna@users.noreply.github.com>
2026-08-09 15:52:33 +0100

    Add files via upload

---
+++ Program - Copy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+
+        while (t-- > 0)
+        {
+            int k = int.Parse(Console.ReadLine());
+            int[] c = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int pairs = 0;
+            bool three = false;
+
+            foreach (int x in c)
+            {
+                if (x >= 3)
+                    three = true;
+
+                if (x >= 2)
+                    pairs++;
+            }
+
+            if (three || pairs >= 2)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cf_div3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] a = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                int x = a[0];
+                int y = a[1];
+                    if(x%y==0)
+                   {
+                        Console.WriteLine("YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+
+                }
+             
+            }
+        }
+    }
+
